261a29b6fc185420897a332cf1380ea39121f8f4
Miwa Tsubasa <mtsubasa@c3r3s1.42tokyo.jp>
2024-10-28 19:21:59 +0900

    ok

--- main/src/ft_print_pointer.c
+++ main/src/ft_print_pointer.c
@@ -6,39 +6,30 @@
 /*   By: mtsubasa <mtsubasa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 23:11:05 by mtsubasa          #+#    #+#             */
-/*   Updated: 2024/10/26 18:28:47 by mtsubasa         ###   ########.fr       */
+/*   Updated: 2024/10/28 18:14:46 by mtsubasa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
-static int	ft_ptr_len(unsigned long long num)//桁数計算
+static int	ft_put_ptr(unsigned long long num)//16進数変換
 {
 	int	len;
 
 	len = 0;
-	while (num != 0)
-	{
-		len++;
-		num = num / 16;
-	}
-	return (len);
-}
-
-static void	ft_put_ptr(unsigned long long num)//16進数変換
-{
 	if (num >= 16)
 	{
-		ft_put_ptr(num / 16);
-		ft_put_ptr(num % 16);
+		len += ft_put_ptr(num / 16);
+		len += ft_put_ptr(num % 16);
 	}
 	else
 	{
 		if (num <= 9)
-			ft_print_char(num + '0');//ASCII変換0123456789
+			len += ft_print_char(num + '0');//ASCII変換0123456789
 		else
-			ft_print_char(num - 10 + 'a');//ASCII変換abcdef
+			len += ft_print_char(num - 10 + 'a');//ASCII変換abcdef
 	}
+	return (len);
 }
 
 int	ft_print_pointer(unsigned long long ptr)
@@ -54,8 +45,7 @@ int	ft_print_pointer(unsigned long long ptr)
 	else
 	{
 		len += ft_print_string("0x");
-		ft_put_ptr(ptr);//16進数変換、出力
-		len += ft_ptr_len(ptr);//桁数計算
+		len += ft_put_ptr(ptr);//16進数変換、出力
 	}
 	return (len);
 }
